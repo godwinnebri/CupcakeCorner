7ecffc4267c10e354af343134fa2fd5a767f9acb
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-11-30 21:18:41 +0100

    async image

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,56 +7,25 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result(trackId: 0, trackName: "", collectionName: "")]
     
     var body: some View {
-        ForEach(results, id: \.trackId) { track in
-            
-            
-            VStack(alignment: .leading) {
-                Text(track.trackName)
-                    .font(.headline.bold())
-                
-                Text(track.collectionName)
+        VStack{
+            AsyncImage(url: URL(string: "https://foundations.projectpythia.org/_images/GitHub-logo.png")) { image in
+                    image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                VStack{
+                    ProgressView()
+                    Text("Placeholder")
+                }
             }
-            
-        }
-        .padding()
-        .task {
-            await loadData()
+            .frame(width: 200, height: 200)
         }
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let(data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-        
-    }
-    
-    
+   
 }
 
 #Preview {
