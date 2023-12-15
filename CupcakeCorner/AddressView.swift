c20794560ffe1b0485f7c061deea297df1ef9337
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-12-15 17:41:13 +0100

    check out view

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct AddressView: View {
-    var order: Order
+    @Bindable var order: Order
     
     var body: some View {
-        Text("Hello, World!")
+        Form{
+            Section{
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip code", text: $order.zip)
+
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckOutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -24,4 +24,37 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return true
+        }
+        
+        return false
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+        
+        //complicated cakes cost more
+        cost += Decimal(type) / 2
+        
+        // $1 per cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        //$0.5 per cake for extra sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
